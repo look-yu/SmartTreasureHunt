f90c877bb32af529ce7a15a13a4520f3193aca22
yu-miaojing <2335567276@qq.com>
2026-01-11 13:39:24 +0800

    添加了test文件

--- common/uitils.py
+++ common/uitils.py
@@ -1,9 +1,24 @@
+#工具类文件
+
+
+
 
 import numpy as np
 import torch
 import random
 import os
 
+#使用统一的随机数种子
+def set_seed(seed):
+    """Set the random seed for reproducibility."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)  #cpu
+    #torch.cuda.manual_seed(seed)  #gpu
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 
 #
 
+                                        
